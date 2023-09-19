c13b5c5ada7de1bca1ed140706e361d3d57a5955
Uczeń - sala 213 <Ucze - sala 213@213-13>
2023-09-19 16:54:25 +0200

    666

--- firsrtConsoleApplication1/firsrtConsoleApplication1.cpp
+++ firsrtConsoleApplication1/firsrtConsoleApplication1.cpp
@@ -1,20 +1,30 @@
-// firsrtConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "elo zelo!\n";
+    std::cout << -59.5 << "gbdugjbh" << true << false << '\n';
+
+    //typ nazwa;
+    int number;
+    number = 5;
+
+    int secondNumber, x;
+    secondNumber = number + 2;
+
+    /*
+    Priorytety operatorów:
+    
+    1.(,) 
+    2.*, /, %
+    3.+,-
+    4.=
+
+    */
+
+    std::cin >> secondNumber >> number;
+
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
