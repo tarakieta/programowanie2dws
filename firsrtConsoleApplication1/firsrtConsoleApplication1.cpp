68470a4a6a80d83883f4e7ad8b65f82a030509ae
tarakieta <programowanie2d@yahoo.com>
2023-09-22 17:24:11 +0200

    gg

--- firsrtConsoleApplication1/firsrtConsoleApplication1.cpp
+++ firsrtConsoleApplication1/firsrtConsoleApplication1.cpp
@@ -1,30 +1,73 @@
-
+#define _USE_MATH_DEFINES
 #include <iostream>
 
-int main()
+void test()
 {
-    std::cout << "elo zelo!\n";
-    std::cout << -59.5 << "gbdugjbh" << true << false << '\n';
+    std::cout << "Hello World!\n";
+    std::cout << -59.5 << 'x' << "ghfghjfghj" << true << false << '\n';
 
     //typ nazwa;
     int number;
     number = 5;
 
-    int secondNumber, x;
+    int secondNumber;
     secondNumber = number + 2;
-
     /*
-    Priorytety operatorów:
-    
-    1.(,) 
-    2.*, /, %
-    3.+,-
-    4.=
+    Priorytet operatorów:
+
+    (, )
+    +, - (unarne)
+    *, /, %
+    +, -
+    =
 
     */
-
+    std::cout << "Komuinikat \n";
     std::cin >> secondNumber >> number;
+}
+
+/*
+Podstawy programowania:
+
+1.
+2. Program obliczaj¹cy pole prostok¹ta.
+3. Program obliczaj¹cy objêtoœæ sto¿ka.
+4. Program obliczaj¹cy pole ko³a.
+5. Program obliczaj¹cy wartoœæ wyra¿enia a^2 + b^2
+6. Program obliczaj¹cy pole trójk¹ta o podstawie b i wysokoœci h
+7. Program obliczaj¹cy objêtoœæ kuli o promieniu r
+8. Program obliczaj¹cy pole trapezu o podstawach a i b oraz wysokoœci h
+9. Program obliczaj¹cy œredni¹ wa¿on¹ trzech liczb z podanymi odpowiednimi wagami w1, w2 i w3.
+*/
+
+//Program obliczaj¹cy œredni¹ arytmetyczn¹ dwóch liczb.
+void task1()
+{
+    double firstNumber, secondNumber;
+    std::cout << "Podaj pierwsz¹ liczbê:\n";
+    std::cin >> firstNumber;
+    std::cout << "Podaj drug¹ liczbê:\n";
+    std::cin >> secondNumber;
+
+    double avarageOfNumbers = (firstNumber + secondNumber) / 2;
+
+    std::cout << "Œrednia tych liczb to: " << avarageOfNumbers << "\n";
+}
+
+//Program obliczaj¹cy pole prostok¹ta:
+void task2()
+{
+
+    double a, b;
+
+    std::cout << "Podaj ";
 
 }
 
-
+int main()
+{
+    setlocale(LC_CTYPE, "polish");
+    //test();
+    task1();
+    //task2();
+}
